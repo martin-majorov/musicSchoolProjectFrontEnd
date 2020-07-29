ee54d3f065e129b0254174b11057585b7714cc99
Martin <33triplets@gmail.com>
2020-07-29 16:26:28 +0300

    additional files

---
+++ src/components/NewPayment.vue
@@ -0,0 +1,40 @@
+<template>
+        <div class="background-green student-info-form">
+        New payment has been added successfully!<br><br>
+    <table>
+        <tr>
+            <td class="table-left">Name:</td>
+            <td class="table-right" ><span class="new-student-text"> {{ name }}</span></td>
+        </tr>
+        <tr>
+            <td class="table-left">Payment date:</td>
+            <td class="table-right"><span class="new-student-text">{{ date }}</span></td>
+        </tr>
+        <tr>
+            <td class="table-left">Payment amount:</td>
+            <td class="table-right"><span class="new-student-text">{{ paymentAmount }}</span></td>
+        </tr>
+    </table>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'NewPayment',
+    props: [
+        'name',
+        'date',
+        'paymentAmount'
+    ]
+}
+</script>
+
+<style>
+
+.new-student-text {
+    font-size: 1.1em;
+    font-weight: bold;
+    padding: 10px;
+}
+
+</style>
---
+++ src/components/StudentName.vue
@@ -0,0 +1,9 @@
+<template>
+    <button class="menu-button" @click="$emit('studentInfo', student)">{{ student.name }} {{student.surname}}</button>
+</template>
+
+<script>
+export default {
+    name: "StudentName"
+}
+</script>
